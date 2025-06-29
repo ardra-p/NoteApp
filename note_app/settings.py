150afd89541acda09651173bd4ef4c46be0c7067
Ardra p <165270747+ardra-p@users.noreply.github.com>
2025-06-29 18:16:59 +0530

    Done

--- note_app/settings.py
+++ note_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=!pe9tkn%f%wojbpsvx4mu_k=x0up$-g_-mxa_ur6!)fn2ylc5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
